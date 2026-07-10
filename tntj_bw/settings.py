3791c97c97afe5372a2615e3102ae493b57ca350
Mohamed-Thawfiq <thawfiq07@gmail.com>
2026-07-10 06:33:13 +0530

    Ready for Render deployment

--- tntj_bw/settings.py
+++ tntj_bw/settings.py
@@ -86,12 +86,12 @@ WSGI_APPLICATION = 'tntj_bw.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv("DB_NAME", "bloodnet_db"),
-        'USER': os.getenv("DB_USER", "root"),
-        'PASSWORD': os.getenv("DB_PASSWORD", ""),
-        'HOST': os.getenv("DB_HOST", "localhost"),
-        'PORT': os.getenv("DB_PORT", "3306"),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
     }
 }
 

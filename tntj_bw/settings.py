59b225d74d3f4730671c87490aa26fa2e62d1c6d
Mohamed-Thawfiq <thawfiq07@gmail.com>
2026-07-10 06:06:31 +0530

    Ready for deployment

--- tntj_bw/settings.py
+++ tntj_bw/settings.py
@@ -135,5 +135,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = '/static/'
+
 AUTH_USER_MODEL='authentication.User'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
